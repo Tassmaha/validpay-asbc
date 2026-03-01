765f063d47a1398d09eeb7e09d167aa9852f4419
Tassmaha <112979102+Tassmaha@users.noreply.github.com>
2026-03-01 21:35:52 +0000

    Change API key for genai configuration
    Updated API key for the generative model configuration.

--- validapay.py
+++ validapay.py
@@ -8,7 +8,7 @@ from PIL import Image
 from openpyxl.styles import PatternFill
 
 # Configuration de l'IA (Utilisez votre clé ici)
-genai.configure(api_key="AIzaSyBVkgBBlGm9bzu2WKjg7EeP1yVg3x_iTds")
+genai.configure(api_key="AIzaSyBPKhHv2FPoWVyEcSQV7YRnJW0qQBMaikY")
 model = genai.GenerativeModel('models/gemini-2.0-flash')
 
 # Configuration de la page
